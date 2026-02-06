07142a09e82aa1dadaa95987f4e94739d30bf498
Atharva Kamble <atharvakamble809@gmail.com>
2026-02-06 22:29:18 +0530

    Create p3_2.java

---
+++ p3_2.java
@@ -0,0 +1,156 @@
+import java.util.*;
+
+class p3_2 {
+    static void bubbleSort(int[] a) {
+        int n = a.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (a[j] > a[j + 1]) {
+                    int t = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = t;
+                }
+            }
+        }
+    }
+
+    static void selectionSort(int[] a) {
+        int n = a.length;
+        for (int i = 0; i < n - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < n; j++) {
+                if (a[j] < a[min]) {
+                    min = j;
+                }
+            }
+            int t = a[min];
+            a[min] = a[i];
+            a[i] = t;
+        }
+    }
+
+    static void insertionSort(int[] a) {
+        int n = a.length;
+        for (int i = 1; i < n; i++) {
+            int key = a[i];
+            int j = i - 1;
+            while (j >= 0 && a[j] > key) {
+                a[j + 1] = a[j];
+                j--;
+            }
+            a[j + 1] = key;
+        }
+    }
+
+    static void quickSort(int[] a, int low, int high) {
+        if (low < high) {
+            int p = partition(a, low, high);
+            quickSort(a, low, p - 1);
+            quickSort(a, p + 1, high);
+        }
+    }
+
+    static int partition(int[] a, int low, int high) {
+        int pivot = a[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (a[j] < pivot) {
+                i++;
+                int t = a[i];
+                a[i] = a[j];
+                a[j] = t;
+            }
+        }
+        int t = a[i + 1];
+        a[i + 1] = a[high];
+        a[high] = t;
+        return i + 1;
+    }
+
+    static void mergeSort(int[] a, int l, int r) {
+        if (l < r) {
+            int m = (l + r) / 2;
+            mergeSort(a, l, m);
+            mergeSort(a, m + 1, r);
+            merge(a, l, m, r);
+        }
+    }
+
+    static void merge(int[] a, int l, int m, int r) {
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        for (int i = 0; i < n1; i++)
+            L[i] = a[l + i];
+        for (int j = 0; j < n2; j++)
+            R[j] = a[m + 1 + j];
+
+        int i = 0, j = 0, k = l;
+
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                a[k++] = L[i++];
+            } else {
+                a[k++] = R[j++];
+            }
+        }
+
+        while (i < n1)
+            a[k++] = L[i++];
+
+        while (j < n2)
+            a[k++] = R[j++];
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter number of elements:");
+        int n = sc.nextInt();
+        int[] a = new int[n];
+
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        System.out.println("Choose sorting technique:");
+        System.out.println("1. Bubble Sort");
+        System.out.println("2. Selection Sort");
+        System.out.println("3. Insertion Sort");
+        System.out.println("4. Quick Sort");
+        System.out.println("5. Merge Sort");
+        int choice = sc.nextInt();
+
+        System.out.println("Sorting in progress...");
+
+        switch (choice) {
+            case 1:
+                bubbleSort(a);
+                break;
+            case 2:
+                selectionSort(a);
+                break;
+            case 3:
+                insertionSort(a);
+                break;
+            case 4:
+                quickSort(a, 0, n - 1);
+                break;
+            case 5:
+                mergeSort(a, 0, n - 1);
+                break;
+            default:
+                System.out.println("Invalid choice");
+                return;
+        }
+
+        System.out.println("Sorted array is:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(a[i] + " ");
+        }
+    }
+}
